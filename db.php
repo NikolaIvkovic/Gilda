86059e60ad4eebde247f8a22d03ffa31fb2a0a2e
Nikola Ivkovic <nikolaivkovic021@gmail.com>
2018-02-12 11:47:55 +0100

    db.php pasword changed to work on current server sanklista.php changed to fix month tab problem ocuring on the last day of the month

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 ﻿<?php
 $opt = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
 try{
-$db = new PDO ('mysql:host=localhost;dbname=gildadb;charset=utf8', 'root', '3lminster', $opt);
+$db = new PDO ('mysql:host=localhost;dbname=gildadb;charset=utf8', 'root', '', $opt);
 }
 catch (Exception $e) {	
 	$_SESSION['errors']['system'][] = $e;
--- includes/sanklista.php
+++ includes/sanklista.php
@@ -2,7 +2,7 @@
 require 'header.php';
 include APP_DIR.'classes/sank_lista.php';
 $tablist = '<ul>';
-$today = new DateTime();
+$today = new DateTime(date('Y-m').'-01');
 $today->sub(new DateInterval('P5M'));
 $tablist.='<li><a href="switch_admin.php?action=listeMesec&month='.$today->format('Y-m').'">'.$today->format('M').'</a></li>';
 for ($i=1; $i<= 5; $i++) {

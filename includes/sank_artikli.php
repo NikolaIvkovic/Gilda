a6fbbd6d46cca8b55b42ec36cf189d29a5c36825
Nikola Ivkovic <nikolaivkovic021@gmail.com>
2018-08-16 10:15:43 +0200

    added noSelect as class and attached it to accordion headers as well as ArtikalTiles

--- includes/sank_artikli.php
+++ includes/sank_artikli.php
@@ -4,7 +4,7 @@ $kategorija = new Classes\Kategorija ($_GET['kat_id']);
 $artikli = $kategorija->getArtikli();
 $tiles = '';
 foreach ($artikli as $artikal) {
-	$tiles.= '<div class="artikalTile"
+	$tiles.= '<div class="artikalTile noSelect"
 					data-art_prodajna = "'.$artikal->getProdajna().'" 
 					style="background-image: url(\''.$artikal->getSlika().'\');"
 					data-art_id ="'.$artikal->getArtId().'"><div style="display:inline-block; padding: 30% 0;">'.$artikal->getNaziv().'</div></div>';
